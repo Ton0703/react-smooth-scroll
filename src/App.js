2fbea05fec6d0dda5b14286591283872e5c87784
Ton0703 <44728377+Ton0703@users.noreply.github.com>
2020-07-16 23:35:02 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const app = useRef()
+  const scroll = useRef()
+
+  const scrollConfigs = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0
+  }
+
+  useEffect(() => {
+      requestAnimationFrame(() => scrolling())
+  }, [])
+
+
+  //body总长度为scroll的长度
+  useEffect(() => {
+      document.body.style.height = `${scroll.current.getBoundingClientRect().height}px`
+  }, [])
+
+  const scrolling = () => {
+     scrollConfigs.current = window.scrollY;
+     scrollConfigs.previous += (scrollConfigs.current - scrollConfigs.previous) * scrollConfigs.ease;
+     scrollConfigs.rounded = Math.round(scrollConfigs.previous * 100) / 100
+
+     scroll.current.style.transform = `translateY(-${scrollConfigs.rounded}px)`
+
+     requestAnimationFrame(() => scrolling())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={app} className="App">
+       <div ref={scroll} className='scroll'>
+         <div className='wrap'>Smooth Scroll</div>
+         <div className='wrap'>Smooth Scroll</div>
+         <div className='wrap'>Smooth Scroll</div>
+         <div className='wrap'>Smooth Scroll</div>
+       </div>
     </div>
   );
 }
